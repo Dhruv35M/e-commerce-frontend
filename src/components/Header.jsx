d80c2b7c85ec3322b6418dd64643a60d39ee7ce6
dhruv mishra <dhruv35m@gmail.com>
2024-05-18 04:24:47 +0530

    add react icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Logo from "./Logo";
+import { GrSearch } from "react-icons/gr";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { FaShoppingCart } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="h-16 shadow-md bg-white fixed w-full z-40">
       <div className=" h-full container mx-auto flex items-center px-4 justify-between">
         <div className="">
-          <Logo w={90} h={50} />
+          <Link to={"/"}>
+            <Logo w={90} h={50} />
+          </Link>
         </div>
 
         <div className="hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2">
@@ -16,19 +22,29 @@ const Header = () => {
             className="w-full outline-none"
           />
           <div className="text-lg min-w-[50px] h-8 bg-blue-600 flex items-center justify-center rounded-r-full text-white">
-            search
+            <GrSearch />
           </div>
         </div>
 
         <div className="flex items-center gap-7">
           <div className="relative flex justify-center"></div>
 
-          <span>cart</span>
+          <Link to={"/cart"} className="text-2xl relative">
+            <span>
+              <FaShoppingCart />
+            </span>
+
+            <div className="bg-blue-600 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-3">
+              <p className="text-sm">1</p>
+            </div>
+          </Link>
 
           <div>
-            <button className="px-3 py-1 rounded-full text-white bg-blue-600 hover:bg-blue-700">
-              Login
-            </button>
+            <Link to={"/login"}>
+              <button className="px-3 py-1 rounded-full text-white bg-blue-600 hover:bg-blue-700">
+                Login
+              </button>
+            </Link>
           </div>
         </div>
       </div>
