b443dab854c4565a99754204f7d6204c56193eff
dhruv mishra <dhruv35m@gmail.com>
2024-05-18 03:43:02 +0530

    add header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import Header from "./componenets/Header";
+import Footer from "./componenets/Footer";
 
 function App() {
   return (
     <>
-      <Outlet />
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </>
   );
 }
