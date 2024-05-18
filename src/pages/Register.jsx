7afd8121ef3d9c75c90d4658679f6b0dff8c0c63
dhruv mishra <dhruv35m@gmail.com>
2024-05-18 12:00:27 +0530

    user register api

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,17 +3,21 @@ import Avatar from "../assests/avatar.png";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import SummaryApi from "../common";
 
 const Register = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [userProfileImg, setuserProfileImg] = useState(null);
+  // const [gender, setGender] = useState("");
   const [data, setData] = useState({
     firstName: "",
     lastName: "",
     email: "",
     password: "",
+    gender: "",
+    profilePic: "",
     confirmPassword: "",
     address: {},
   });
@@ -22,18 +26,30 @@ const Register = () => {
 
   const handleOnChange = (e) => {
     const { name, value } = e.target;
-
-    setData((prev) => {
-      return {
-        ...prev,
-        [name]: value,
-      };
-    });
+    setData((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleProfileImgUpload = async (e) => {
+  const handleProfileImgUpdate = async (e) => {
     setuserProfileImg(e.target.files[0]);
     console.log(e.target.files[0]);
+  };
+
+  const handleImgUpload = async (formDataImg) => {
+    try {
+      const response = await fetch(SummaryApi.ImageUpload.url, {
+        method: SummaryApi.ImageUpload.method,
+        body: formDataImg,
+      });
+
+      if (response.ok) {
+        const imgResponse = await response.json();
+        return imgResponse.url;
+      } else {
+        console.error("Failed to upload image");
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
   };
 
   const handleSubmit = async (e) => {
@@ -47,14 +63,23 @@ const Register = () => {
       !data.confirmPassword
     ) {
       console.log("All feilds are required!");
+      toast("All feilds are required!");
+      return;
     }
 
     if (data.password !== data.confirmPassword) {
-      console.log("Please check password and confirm password");
+      toast.error("Please check password and confirm password");
       return;
     }
 
     try {
+      const formData = new FormData();
+      if (userProfileImg) {
+        formData.append("image", userProfileImg);
+        let imgUrl = await handleImgUpload(formData);
+        if (imgUrl) data.profilePic = imgUrl;
+      }
+
       const responseData = await fetch(SummaryApi.Register.url, {
         method: SummaryApi.Register.method,
         headers: {
@@ -64,13 +89,20 @@ const Register = () => {
       });
 
       const response = await responseData.json();
-      if (response) {
-        console.log("registered");
-        navigate("/");
-      } else {
-        console.log("failed to register");
+
+      if (!response.status === 201) {
+        toast.error(response.message);
+        console.log(response.message);
+        return;
       }
+
+      localStorage.setItem("token", response.token);
+      localStorage.setItem("userId", response.userId);
+      toast.success(response);
+      console.log({ data, response });
+      navigate("/");
     } catch (err) {
+      toast.error(err);
       console.log(err);
     }
   };
@@ -102,7 +134,7 @@ const Register = () => {
                   <input
                     type="file"
                     className="hidden"
-                    onChange={handleProfileImgUpload}
+                    onChange={handleProfileImgUpdate}
                   />
                 </label>
               </form>
@@ -139,7 +171,6 @@ const Register = () => {
                   </div>
                 </div>
               </div>
-
               <div className="grid">
                 <label>Email: </label>
                 <div className="bg-slate-100 p-2">
@@ -152,6 +183,47 @@ const Register = () => {
                     required
                     className="w-full h-full outline-none bg-transparent"
                   />
+                </div>
+              </div>
+              <div className="">
+                Gender:
+                <div className="flex gap-5 justify-start">
+                  <label className="cursor-pointer flex items-center">
+                    <input
+                      type="radio"
+                      name="gender"
+                      value="male"
+                      checked={data.gender === "male"}
+                      onChange={handleOnChange}
+                      className="mr-2"
+                      required
+                    />
+                    <p>male</p>
+                  </label>
+                  <label className="cursor-pointer flex items-center">
+                    <input
+                      type="radio"
+                      name="gender"
+                      value="female"
+                      checked={data.gender === "female"}
+                      onChange={handleOnChange}
+                      className="mr-2"
+                      required
+                    />
+                    <p>female</p>
+                  </label>
+                  <label className="cursor-pointer flex items-center">
+                    <input
+                      type="radio"
+                      name="gender"
+                      value="others"
+                      checked={data.gender === "others"}
+                      onChange={handleOnChange}
+                      className="mr-2"
+                      required
+                    />
+                    <p>others</p>
+                  </label>
                 </div>
               </div>
 
@@ -175,7 +247,6 @@ const Register = () => {
                   </div>
                 </div>
               </div>
-
               <div>
                 <label>Confirm Password: </label>
                 <div className="bg-slate-100 p-2 flex">
@@ -199,7 +270,6 @@ const Register = () => {
                   </div>
                 </div>
               </div>
-
               <button className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6">
                 Register
               </button>
